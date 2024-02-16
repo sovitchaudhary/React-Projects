fe52362a4096dd697aa2bf0a4d5153c8ce5a8d29
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-02-16 10:30:39 +0530

    Design Navigation Bar

--- moviesite/src/components/Navbar.jsx
+++ moviesite/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="topnav"><a href="#">MovieSite</a></div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+        <button><a href="#">Sign In</a></button>
+        <button><a href="#">Sign Up</a></button>
+      </ul> 
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
